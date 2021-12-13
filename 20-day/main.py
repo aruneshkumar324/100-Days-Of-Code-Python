1c464533f9e04ad9e2bf678bc248592761397ae7
Arunesh kumar <aruneshkumar324@gmail.com>
2021-12-13 17:30:43 +0530

    20-Day Complete

--- 20-day/main.py
+++ 20-day/main.py
@@ -9,8 +9,16 @@ screen.title("Snake Game")
 screen.bgcolor("black")
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
 
 snake = Snake()
+
+# KEY CONTROL
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 
 
 game_is_on = True
