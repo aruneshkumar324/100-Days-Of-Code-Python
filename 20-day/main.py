698b642a0c47d49d7e8898164098f38e0b6598e9
Arunesh kumar <aruneshkumar324@gmail.com>
2021-12-14 18:00:55 +0530

    21-Day : Increase tail & delect tail touch each other

--- 20-day/main.py
+++ 20-day/main.py
@@ -33,12 +33,21 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend_tail()
         score.increase_score()
 
     # GAME OVER
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         score.game_over()
+
+    # check tail touch
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
 
 
 screen.exitonclick()
