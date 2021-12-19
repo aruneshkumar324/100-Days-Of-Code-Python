c7610436d0fffd87458134be0c9feb8da6efc4d4
Arunesh kumar <aruneshkumar324@gmail.com>
2021-12-19 22:29:16 +0530

    23-Day : Player move on keypress

---
+++ 23-day/player.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.turtle_move = -280
+        self.shape("turtle")
+        self.setheading(90)
+        self.penup()
+        self.goto(0, self.turtle_move)
+
+    def move(self):
+        self.turtle_move += 10
+        self.goto(0, self.turtle_move)
