82d5d75c907ecbf85c299a07789c130ca7388e85
Arunesh kumar <aruneshkumar324@gmail.com>
2022-01-07 19:03:13 +0530

    25 - Day : My solution ( BUG )

--- 25-Day/us-states-game-start/main.py
+++ 25-Day/us-states-game-start/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -7,14 +8,30 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = turtle.textinput(title="Guess the States", prompt="What's another state's name?")
+score = 0
 
-# def get_mouse_click_coor(x, y):
-#     print(x, y)
-#
-# turtle.onscreenclick(get_mouse_click_coor)
+game_on = True
 
-# turtle.mainloop()
+while game_on:
+    if score <= 50:
+        answer_state = turtle.textinput(title=f"{score}/50 States Correct", prompt="What's another state's name?").title()
 
+        data = pandas.read_csv("50_states.csv")
 
-print(answer_state)
+        state_data = data[data.state == answer_state]
+
+        state_count = data[data.state == answer_state].count()
+
+        state_count = state_count.state
+
+        x_value = float(state_data.x)
+        y_value = float(state_data.y)
+        print(state_data)
+        turtle.penup()
+        turtle.goto(x_value, y_value)
+        turtle.write(answer_state, align="center", font=('Arial', 12, 'normal'))
+
+        score += 1
+    else:
+        game_on = False
+
