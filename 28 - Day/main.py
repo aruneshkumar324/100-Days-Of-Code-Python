64ac24008f8ac3453a38cea1bafd42eda9a9fc0c
Arunesh kumar <aruneshkumar324@gmail.com>
2022-01-14 23:42:55 +0530

    28-Day : C - Timer count down

--- 28 - Day/main.py
+++ 28 - Day/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -13,9 +14,17 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(5 * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -23,11 +32,16 @@ window = Tk()
 window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# def demo(data, a, b, c):
+#     print(data, a, b, c)
+#
+# window.after(2000, demo, 'Data', 1, 2, 3)
+
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas_photo = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=canvas_photo)
-canvas.create_text(100, 130, text='00:00', font=(FONT_NAME, 15, 'bold'), fill='white')
+timer_text = canvas.create_text(100, 130, text='00:00', font=(FONT_NAME, 35, 'bold'), fill='white')
 canvas.grid(column=1, row=1)
 
 
@@ -35,7 +49,7 @@ timer_label = Label(text='Timer', font=(FONT_NAME, 50, 'bold'), fg=GREEN, bg=YEL
 timer_label.grid(column=1, row=0)
 
 
-start_button = Button(text='Start', highlightthickness=0)
+start_button = Button(text='Start', command=start_timer, highlightthickness=0)
 start_button.grid(column=0, row=2)
 
 
@@ -45,6 +59,30 @@ reset_button.grid(column=2, row=2)
 
 check_label = Label(text='✔', font=(FONT_NAME, 12, 'bold'), fg=GREEN, bg=YELLOW)
 check_label.grid(column=1, row=3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
