6f769dc44148c2bda635bee760e93b92a702c83b
Arunesh kumar <aruneshkumar324@gmail.com>
2022-01-18 01:57:17 +0530

    31-Day : 3 Step ( Flip card and show English content )

--- 31-Day/main.py
+++ 31-Day/main.py
@@ -2,33 +2,41 @@ from tkinter import *
 import pandas
 from random import randint, choice
 
+import time
+
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+def demo():
+    print('first')
+    time.sleep(2)
+    print('second')
+
+# demo()
 
 
 # -----------------------------     READ DATA FROM FILE      --------------------------------
 
-# #  MY SOLUTION
-# def generate_word():
-#     data = pandas.read_csv('data/french_words.csv')
-#     french = pandas.DataFrame(data)
-#     french_word = french['French'][randint(0, 100)]
-#     print(french_word)
-#     canvas.itemconfig(update_text, text=french_word)
-
 data = pandas.read_csv('data/french_words.csv')
-print(data)
-
-# to_learn = data.to_dict()
 to_learn = data.to_dict(orient='records')
-print(to_learn)
+
+current_card = {}
 
 
 def next_card():
+    global current_card, timer_flip
+    window.after_cancel(timer_flip)
     current_card = choice(to_learn)
-    print(current_card)
-    canvas.itemconfig(update_title, text='French')
-    canvas.itemconfig(update_word, text=current_card['French'])
+    canvas.itemconfig(update_title, text='French', fill="black")
+    canvas.itemconfig(update_word, text=current_card['French'], fill="black")
+    canvas.itemconfig(canvas_image, image=front_image)
+    timer_flip = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(canvas_image, image=back_image)
+    canvas.itemconfig(update_title, text="English", fill="white")
+    canvas.itemconfig(update_word, text=current_card['English'], fill="white")
 
 
 # -----------------------------     UI      --------------------------------
@@ -36,10 +44,14 @@ window = Tk()
 window.title('Flashy')
 window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
 
+timer_flip = window.after(3000, func=flip_card)
+
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-card_image = PhotoImage(file='images/card_front.png')
-canvas.create_image(400, 263, image=card_image)
+front_image = PhotoImage(file='images/card_front.png')
+back_image = PhotoImage(file="images/card_back.png")
+
+canvas_image = canvas.create_image(400, 263, image=front_image)
 update_title = canvas.create_text(400, 150, text='French', font=('Arial', 40, 'italic'))
 update_word = canvas.create_text(400, 250, text='word', font=('Arial', 60, 'bold'))
 canvas.grid(column=0, row=0, columnspan=2)
@@ -56,8 +68,6 @@ right_button.grid(column=1, row=1)
 
 
 next_card()
-
-
 
 
 
