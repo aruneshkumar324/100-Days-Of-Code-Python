fd3d2b025eec53350f939d44f03c74cec2ec748c
Arunesh kumar <aruneshkumar324@gmail.com>
2022-01-17 23:09:08 +0530

    31-Day : 2 Step ( Generate random work and display on UI display )

--- 31-Day/main.py
+++ 31-Day/main.py
@@ -1,7 +1,34 @@
 from tkinter import *
+import pandas
+from random import randint, choice
 
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+
+# -----------------------------     READ DATA FROM FILE      --------------------------------
+
+# #  MY SOLUTION
+# def generate_word():
+#     data = pandas.read_csv('data/french_words.csv')
+#     french = pandas.DataFrame(data)
+#     french_word = french['French'][randint(0, 100)]
+#     print(french_word)
+#     canvas.itemconfig(update_text, text=french_word)
+
+data = pandas.read_csv('data/french_words.csv')
+print(data)
+
+# to_learn = data.to_dict()
+to_learn = data.to_dict(orient='records')
+print(to_learn)
+
+
+def next_card():
+    current_card = choice(to_learn)
+    print(current_card)
+    canvas.itemconfig(update_title, text='French')
+    canvas.itemconfig(update_word, text=current_card['French'])
 
 
 # -----------------------------     UI      --------------------------------
@@ -13,22 +40,22 @@ window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_image = PhotoImage(file='images/card_front.png')
 canvas.create_image(400, 263, image=card_image)
-canvas.create_text(400, 150, text='Title', font=('Arial', 40, 'italic'))
-canvas.create_text(400, 250, text='Word', font=('Arial', 60, 'bold'))
+update_title = canvas.create_text(400, 150, text='French', font=('Arial', 40, 'italic'))
+update_word = canvas.create_text(400, 250, text='word', font=('Arial', 60, 'bold'))
 canvas.grid(column=0, row=0, columnspan=2)
 
 
 wrong_image = PhotoImage(file='images/wrong.png')
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
 
 right_image = PhotoImage(file='images/right.png')
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command=next_card)
 right_button.grid(column=1, row=1)
 
 
-
+next_card()
 
 
 
