e15cae71b978c96342e2231b89bf086c35a78975
Arunesh kumar <aruneshkumar324@gmail.com>
2022-01-22 21:58:55 +0530

    34-Day : UI setup ( ui.py and main.py )

--- 34-Day/ui.py
+++ 34-Day/ui.py
@@ -1,2 +1,30 @@
-THEME_COLOR = "#375362"
+from tkinter import *
 
+
+THEME_COLOR = "#375362"
+score = 0
+
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzer")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text=f"Score : {score}", font=("Arial", 12), bg=THEME_COLOR, fg="white")
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250)
+        self.canvas.create_text(150, 125, text="Question", font=("Arial", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=20)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
