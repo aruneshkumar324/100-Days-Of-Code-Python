80c613198713c79b284409f0a4b4ace60de0b4c4
Arunesh kumar <aruneshkumar324@gmail.com>
2022-01-22 23:58:49 +0530

    34-Day : E - Complete Project

--- 34-Day/ui.py
+++ 34-Day/ui.py
@@ -14,10 +14,10 @@ class QuizInterface:
         self.window.title("Quizzer")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        self.score_label = Label(text=f"Score : {score}", font=("Arial", 12), bg=THEME_COLOR, fg="white")
+        self.score_label = Label(text="Score : 0", font=("Arial", 12), bg=THEME_COLOR, fg="white")
         self.score_label.grid(column=1, row=0)
 
-        self.canvas = Canvas(width=300, height=250)
+        self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(150, 125, width=280, text="Question", font=("Arial", 20, "italic"))
         self.canvas.grid(column=0, row=1, columnspan=2, pady=20)
 
@@ -42,12 +42,27 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfigure(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            self.canvas.config(bg="white")
+            self.score_label.config(text=f"Score : {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfigure(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfigure(self.question_text, text="Quiz Over")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+            self.canvas.config(bg="white")
+
 
     def true_answer(self):
-        self.quiz.check_answer("True")
+        self.give_feedback(self.quiz.check_answer("True"))
 
     def false_answer(self):
-        self.quiz.check_answer("False")
+        self.give_feedback(self.quiz.check_answer("False"))
 
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
