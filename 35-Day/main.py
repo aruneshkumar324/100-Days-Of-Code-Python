911e5345e85c18122357fcaf9d640f8c05560deb
Arunesh kumar <aruneshkumar324@gmail.com>
2022-01-24 03:15:59 +0530

    35-Day : C - Complete

--- 35-Day/main.py
+++ 35-Day/main.py
@@ -1,9 +1,13 @@
 import requests
+from twilio.rest import Client
+
 
 # "lat": 13.046396,
 #     "lon": 77.723328,
 
 API = "6940d199dd16f53e04bbc0f5896b6c44"
+account_sid = "ACfb9a6b8aa9e4675b0d2abd5179828bb6"
+auth_token = "f4e0af510b8c63e2afd1ec3d042aa0c0"
 
 parameters = {
     "lat": -12.462320,
@@ -19,27 +23,10 @@ response.raise_for_status()
 data = response.json()
 hourly_data = data["hourly"]
 
-print(hourly_data)
 
 b = hourly_data[0]["weather"][0]["id"]
-# print(b)
 
-
-# # MY SOLUTION
-# will_rain = False
-#
-# for x in range(0, 12):
-#     code = hourly_data[x]["weather"][0]["id"]
-#     if code < 700:
-#         will_rain = True
-#
-# if will_rain:
-#     print("Bring Umbrella")
-
-
-# # ANGELA SOLUTION
 weather_slice = data["hourly"][:12]
-print(weather_slice)
 
 will_rain = False
 
@@ -49,17 +36,13 @@ for x in weather_slice:
         will_rain = True
 
 if will_rain:
-    print("Bring Umbrella")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="Bring Umbrella",
+        from_='+16165800849',
+        to="+9189718 18410"
+    )
 
-
-
-
-
-
-
-
-
-
-
-
+    print("Message Sent")
 
