9ba0d4eefe25f27d85175382d4c3a48a79f0cbd3
Arunesh kumar <aruneshkumar324@gmail.com>
2022-01-24 22:46:48 +0530

    36-Day : Accounding to Angela Solution

--- 36-Day/main.py
+++ 36-Day/main.py
@@ -8,64 +8,64 @@ NEWS_API = "80f6068367f2474e8a0e691e555237ea"
 ACCOUNT_SID = "ACfb9a6b8aa9e4675b0d2abd5179828bb6"
 AUTH_TOKEN = "bc6c1bff35b16f91222411b8793112f9"
 
+
+# PARAMETERS
 stock_parameters = {
     "function": "TIME_SERIES_DAILY",
     "symbol": "TSLA",
     "apikey": STOCK_API
 }
 
-
-news_parameters = {
-    "q": "Tesla",
-    "from": "2022-01-24",
-    "sortBy": "popularity",
-    "apiKey": NEWS_API
-}
-
-
+# STOCK DATA
 stock_response = requests.get("https://www.alphavantage.co/query", params=stock_parameters)
 stock_response.raise_for_status()
 
 data = stock_response.json()["Time Series (Daily)"]
-today_close = data["2022-01-21"]["4. close"]
+data_list = [value for (key, value) in data.items()]
+print(data_list)
+
+today_close = data_list[0]["4. close"]
 print(today_close)
 
-yesterday_close = data["2022-01-20"]["4. close"]
+yesterday_close = data_list[1]["4. close"]
 print(yesterday_close)
 diff_total = float(today_close) - float(yesterday_close)
-print(diff_total)
 
-in_percentage = -5.26
-mess = ""
-if in_percentage > 0:
-    mess = f"TSLA up over {in_percentage}"
+emoji = None
+
+if diff_total > 0:
+    emoji = '⬆️'
 else:
-    mess = f"TSLA down over {in_percentage}"
+    emoji = '⬇️'
 
-print(mess)
-
-
-news_response = requests.get("https://newsapi.org/v2/everything", params=news_parameters)
-news_response.raise_for_status()
-
-news_data = news_response.json()["articles"][0]
-
-headline = news_data["title"]
-print(headline)
-
-brief = news_data["description"]
-print(brief)
+in_percentage = round((diff_total / float(yesterday_close)) * 100)
 
 
-send_message = f"{mess}\nHeadline: {headline}\nBrief: {brief}"
-print(send_message)
+if abs(in_percentage) > 1:
+    # NEWS DATA
+    news_parameters = {
+        "q": "Tesla",
+        "apiKey": NEWS_API
+    }
 
-client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    news_response = requests.get("https://newsapi.org/v2/everything", params=news_parameters)
+    news_response.raise_for_status()
 
-client.messages.create(
-                     body=send_message,
-                     from_='+16165800849',
-                     to='+918971818410'
-)
+    news = news_response.json()["articles"]
+    articles = news[:3]
 
-print("Sent")
+    print(articles)
+
+    # SEND MESSAGE
+    formatted_text = [f"TSLA: {emoji}{diff_total}%\nHeadline: {article['title']}\nBrief: {article['description']}" for article in articles]
+
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+    for article in formatted_text:
+        client.messages.create(
+                             body=article,
+                             from_='+16165800849',
+                             to='+918971818410'
+        )
+
+        print("Sent")
