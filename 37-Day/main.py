6853d1233a732ca2dd27100e9073b5f08a8c55bb
Arunesh kumar <aruneshkumar324@gmail.com>
2022-01-25 21:46:04 +0530

    37-Day : 2 - Create graph

--- 37-Day/main.py
+++ 37-Day/main.py
@@ -2,14 +2,56 @@ import requests
 
 
 URL = "https://pixe.la/v1/users"
+USERNAME = "arunesh"
+TOKEN = "32ewu3dsh34jwe3fdgh34fds4hgfd4we43"
 
 parameters = {
-    "token": "32ewu3dsh34jwe3fdgh34fds4hgfd4we43",
-    "username": "arunesh",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(URL, json=parameters)
+# response = requests.post(URL, json=parameters)
+# print(response.text)
+
+
+# CREATE GRAPH
+
+GRAPH_URL = f"{URL}/{USERNAME}/graphs"
+
+graph_parameter = {
+    "id": "graph1",
+    "name": "Cycle Graph",
+    "unit": "km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+graph_header = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=GRAPH_URL, json=graph_parameter, headers=graph_header)
 
 print(response.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
