68aa178cdeac2d01ac740b1fe798d4e62f2b6d9e
Arunesh kumar <aruneshkumar324@gmail.com>
2022-01-28 19:52:12 +0530

    45-DAy : Complete with final project - Movie title scraping

---
+++ 45-Day/best_movies.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+movies_response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+response = movies_response.text
+
+
+soup = BeautifulSoup(response, 'html.parser')
+
+title = soup.find_all(name='h3', class_='title')
+titles = []
+for x in title[::-1]:
+    titles.append(x.getText())
+
+
+with open('movies.txt', mode='a') as movies_title:
+    for x in titles:
+        movies_title.write(f"{x}\n")
