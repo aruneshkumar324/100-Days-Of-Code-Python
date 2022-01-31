76e805ea772a5271b9e9735e34442e7b51bbb0fa
Arunesh kumar <aruneshkumar324@gmail.com>
2022-01-31 21:23:18 +0530

    55-Day : Starting of FLASK

---
+++ 54-Day/practice.py
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# def delay_decorator(function):
+#     def first():
+#         sleep(2)
+#         function()
+#         function()
+#
+#     return first
+#
+#
+# @delay_decorator
+# def morning():
+#     print('morninig')
+#
+#
+# morning()
