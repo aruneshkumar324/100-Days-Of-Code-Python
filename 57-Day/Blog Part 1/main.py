aa610d275aeaf040f9f6aad0dd982aa43a9d9a01
Arunesh kumar <aruneshkumar324@gmail.com>
2022-02-02 02:23:39 +0530

    57-Day : C - My Solution

--- 57-Day/Blog Part 1/main.py
+++ 57-Day/Blog Part 1/main.py
@@ -2,19 +2,21 @@ from flask import Flask, render_template
 import requests
 
 
+response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+all_posts = response.json()
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
-    all_posts = response.json()
     return render_template("index.html", posts=all_posts)
 
 
 @app.route('/post/<id>')
 def post(id):
-    return render_template('post.html')
+    id = int(id)
+    return render_template('post.html', posts=all_posts, post_id=id)
 
 
 if __name__ == "__main__":
--- 57-Day/Blog Part 1/post.py
+++ 57-Day/Blog Part 1/post.py
@@ -1,8 +1,3 @@
-import requests
-
-response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
-
-
 class Post:
     def __init__(self):
         pass
