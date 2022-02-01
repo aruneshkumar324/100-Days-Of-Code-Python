5f0a1a2c9670bb1c0181a4dbcdb621bb730dfaa5
Arunesh kumar <aruneshkumar324@gmail.com>
2022-02-02 02:03:02 +0530

    57-Day : B - Showing data on page & url link also

--- 57-Day/Blog Part 1/main.py
+++ 57-Day/Blog Part 1/main.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+    all_posts = response.json()
+    return render_template("index.html", posts=all_posts)
+
+
+@app.route('/post/<id>')
+def post(id):
+    return render_template('post.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- 57-Day/Blog Part 1/post.py
+++ 57-Day/Blog Part 1/post.py
@@ -1,2 +1,8 @@
+import requests
+
+response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+
+
 class Post:
-    pass
+    def __init__(self):
+        pass
