c104dd06fa4ecb550da8bca08dc23f9f1754784a
Arunesh kumar <aruneshkumar324@gmail.com>
2022-02-01 23:56:57 +0530

    57-Day : Complete

---
+++ 57-Day/Blog_web/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route('/blog/<num>')
+def get_blog(num):
+    print(num)
+    response = requests.get(url='https://api.npoint.io/c790b4d5cab58020d391')
+    all_posts = response.json()
+    return render_template('blog.html', posts=all_posts)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
