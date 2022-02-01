01b0095229f3ae49c73c22c008f382e2e3c3677b
Arunesh kumar <aruneshkumar324@gmail.com>
2022-02-02 02:46:33 +0530

    57-Day : D - Angela Solution ( Complete )

--- 57-Day/Blog Part 1/main.py
+++ 57-Day/Blog Part 1/main.py
@@ -1,22 +1,32 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 
 
 response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
-all_posts = response.json()
+posts = response.json()
+
+post_objects = []
+for post in posts:
+    post_obj = Post(post['id'], post['title'], post['subtitle'], post['body'])
+    post_objects.append(post_obj)
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html", posts=all_posts)
+    return render_template("index.html", posts=post_objects)
 
 
-@app.route('/post/<id>')
-def post(id):
-    id = int(id)
-    return render_template('post.html', posts=all_posts, post_id=id)
+@app.route('/post/<int:index>')
+def post(index):
+    requested_post = None
+    for post in post_objects:
+        if post.id == index:
+            requested_post = post
+    return render_template('post.html', post=requested_post)
 
 
 if __name__ == "__main__":
