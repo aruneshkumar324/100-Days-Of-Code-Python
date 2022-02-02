4f17b65e7d57b18fb10a12baf00e2d8c793ae58a
Arunesh kumar <aruneshkumar324@gmail.com>
2022-02-02 21:05:58 +0530

    59-Day : C - Showing post on home page

--- 59-Day/main.py
+++ 59-Day/main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
+response = requests.get(url="https://api.npoint.io/9d474babfbdf3a487bf2").json()
+
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', posts=response)
 
 
 @app.route('/about')
