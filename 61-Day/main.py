b3c8b5b1b119fa8bf3cf234f77654dc6c2b88014
Arunesh kumar <aruneshkumar324@gmail.com>
2022-02-04 02:26:27 +0530

    61-Day : D - Form validation

--- 61-Day/main.py
+++ 61-Day/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, Length
+
+# i have insllated -> email_validator
 
 
 app = Flask(__name__)
@@ -10,8 +12,8 @@ app.secret_key = 'helloarunesh'
 
 
 class Login(FlaskForm):
-    email = StringField(label='Email : ', validators=[DataRequired()])
-    password = PasswordField(label='Password : ', validators=[DataRequired()])
+    email = EmailField(label='Email : ', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password : ', validators=[DataRequired(), Length(min=8, message='Field must be at least 8 characters long.')])
     submit = SubmitField(label='Log in')
 
 
@@ -23,6 +25,7 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = Login()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 
